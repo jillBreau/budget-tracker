e02bb66ec8a07d28c40dea1bba4a101932cb8b0d
Jillian <jillian@Jillians-MacBook-Pro.local>
2020-08-09 10:46:09 -0230

    Updates

---
+++ precache-manifest.aa4b957d2586ce51a49e021cdae1634a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2c0ad4ef598814331dd453ffa08e34",
+    "url": "/budget-tracker/index.html"
+  },
+  {
+    "revision": "ada1b1621514e55d9562",
+    "url": "/budget-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b80260afe50c8e931e03",
+    "url": "/budget-tracker/static/js/2.b2bb315f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-tracker/static/js/2.b2bb315f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada1b1621514e55d9562",
+    "url": "/budget-tracker/static/js/main.47d663ba.chunk.js"
+  },
+  {
+    "revision": "bc515d496ebdae6bf221",
+    "url": "/budget-tracker/static/js/runtime-main.9010eaf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/budget-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
